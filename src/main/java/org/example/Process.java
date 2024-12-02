a0c9f902a6be7598e93ad872ab3825ffa10056c9
ahmeddkhaled1 <ahmeddkhaled951@gmail.com>
2024-12-02 17:41:52 +0200

    Initiating Process class

--- src/main/java/org/example/Process.java
+++ src/main/java/org/example/Process.java
@@ -1,4 +1,56 @@
 package org.example;
 
 public class Process {
+    private String name;
+    private int arrivalTime;
+    private int burstTime;
+    private int initialBurstTime;
+    private int priority;
+    private int turnRoundTime;
+    private int waitingTime;
+    private boolean startedExecution;
+
+    public Process(String n, int a,int b,int p){
+        name = n;
+        arrivalTime = a;
+        initialBurstTime = b;
+        burstTime = b;
+        priority = p;
+    }
+
+    public int getInitialBurstTime() {
+        return initialBurstTime;
+    }
+
+    public void setBurstTime(int b){
+        burstTime = b;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getArrivalTime(){
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getPriority(){
+        return priority;
+    }
+
+    public int getTurnRoundTime(){
+        return turnRoundTime;
+    }
+
+    public int getWaitingTime(){
+        return waitingTime;
+    }
+
+    public boolean hasStartedExecution() {
+        return startedExecution;
+    }
 }
