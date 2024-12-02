396bce364ec31d6ff4c67df0f72f5a10ec226659
ahmeddkhaled1 <ahmeddkhaled951@gmail.com>
2024-12-02 17:42:17 +0200

    Updating Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,32 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of processes: ");
+        int numOfProcesses = scanner.nextInt();
+        Process[] processes = new Process[numOfProcesses];
 
+        for(int i = 0; i < numOfProcesses; i++) {
+            System.out.println("Enter details of process " + i + ": ");
+
+            scanner.nextLine();
+            System.out.println("Enter name of process");
+            String name = scanner.nextLine();
+            System.out.println("Enter arrival time ");
+            int arrivalTime = scanner.nextInt();
+            System.out.println("Enter initial burst time ");
+            int initialBurstTime = scanner.nextInt();
+            System.out.println("Enter priority");
+            int priority = scanner.nextInt();
+
+            processes[i] = new Process(name,arrivalTime,initialBurstTime,priority);
+
+
+        }
     }
 }
