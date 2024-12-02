d1ba3e501508b62a6941035219b1069cb5f480af
ahmeddkhaled1 <ahmeddkhaled951@gmail.com>
2024-12-03 00:02:53 +0200

    Update Process

--- src/main/java/org/example/Process.java
+++ src/main/java/org/example/Process.java
@@ -3,24 +3,52 @@ package org.example;
 public class Process {
     private String name;
     private int arrivalTime;
+    private int remainingBurstTime;
     private int burstTime;
-    private int initialBurstTime;
     private int priority;
     private int turnRoundTime;
     private int waitingTime;
     private boolean startedExecution;
+    private int fcaiFactor;
+    private int Quantum;
+    private int executedTime;
+
 
     public Process(String n, int a,int b,int p){
         name = n;
         arrivalTime = a;
-        initialBurstTime = b;
         burstTime = b;
+        remainingBurstTime = b;
         priority = p;
     }
 
-    public int getInitialBurstTime() {
-        return initialBurstTime;
+    void setExecutedTime(int t){
+        executedTime = t;
     }
+
+    public int getExecutedTime(){
+        return executedTime;
+    }
+
+    void setFcaiFcator(int factor){
+        fcaiFactor = factor;
+    };
+
+    public int getFcaiFactor(){
+        return fcaiFactor;
+    };
+
+    void setQuantum(int quantum){
+        Quantum = quantum;
+    }
+
+    public int getQuantum(){
+        return Quantum;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    };
 
     public void setBurstTime(int b){
         burstTime = b;
@@ -34,8 +62,8 @@ public class Process {
         return arrivalTime;
     }
 
-    public int getBurstTime() {
-        return burstTime;
+    public int getReamininBurstTime() {
+        return remainingBurstTime;
     }
 
     public int getPriority(){
