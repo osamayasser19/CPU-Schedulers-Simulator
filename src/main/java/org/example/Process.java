7f5a62d03456b0d5b4e045708f23ae2d1c88afad
raghd-ahmed <147190201+raghd-ahmed@users.noreply.github.com>
2024-12-03 18:27:17 +0200

    Update Process.java

--- src/main/java/org/example/Process.java
+++ src/main/java/org/example/Process.java
@@ -1,20 +1,18 @@
 package org.example;
-
 public class Process {
-    public String name;
-    public int arrivalTime;
+    String name;
+    int arrivalTime;
     public int remainingBurstTime;
-    public int burstTime;
-    public int priority;
-    public int turnRoundTime;
-    public int waitingTime;
-    public boolean startedExecution;
-    public int fcaiFactor;
-    public int Quantum;
-    public int executedTime;
-
-
-    public Process(String n, int a,int b,int p){
+    int burstTime;
+    int priority;
+    int turnRoundTime;
+    int waitingTime;
+    public boolean startedExecution= false;
+    private int fcaiFactor;
+    private int Quantum;
+    private int executedTime=-1;
+    private int completionTime;
+    public Process(String n, int a, int b, int p) {
         name = n;
         arrivalTime = a;
         burstTime = b;
@@ -22,43 +20,54 @@ public class Process {
         priority = p;
     }
 
-    void setExecutedTime(int t){
+    void setExecutedTime(int t) {
         executedTime = t;
     }
 
-    public int getExecutedTime(){
+    public int getExecutedTime() {
         return executedTime;
     }
 
-    void setFcaiFcator(int factor){
+    void setFcaiFcator(int factor) {
         fcaiFactor = factor;
-    };
+    }
 
-    public int getFcaiFactor(){
+    ;
+
+    public int getFcaiFactor() {
         return fcaiFactor;
-    };
+    }
 
-    void setQuantum(int quantum){
+    ;
+
+    void setQuantum(int quantum) {
         Quantum = quantum;
     }
 
-    public int getQuantum(){
+    public int getQuantum() {
         return Quantum;
     }
 
     public int getBurstTime() {
         return burstTime;
-    };
+    }
+    public void setTurnRoundTime(int turnRoundTime) {
+        this.turnRoundTime = turnRoundTime;
+    }
 
-    public void setBurstTime(int b){
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public void setBurstTime(int b) {
         burstTime = b;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getArrivalTime(){
+    public int getArrivalTime() {
         return arrivalTime;
     }
 
@@ -66,22 +75,30 @@ public class Process {
         return remainingBurstTime;
     }
 
-    void setRemainingBurstTime(int t){
-        remainingBurstTime = t;
-    }
-
-    public int getPriority(){
+    public int getPriority() {
         return priority;
     }
 
-    public int getTurnRoundTime(){
+    public int getTurnRoundTime() {
         return turnRoundTime;
     }
 
-    public int getWaitingTime(){
+    public int getWaitingTime() {
         return waitingTime;
     }
 
+    // New method to set the completion time
+    public void setCompletionTime(int time) {
+        this.completionTime = time;
+    }
+    // New method to get the completion time
+    public int getCompletionTime() {
+        return completionTime;
+    }
+    // New method to mark that the process has started execution
+    public void setStartedExecution(boolean started) {
+        this.startedExecution = started;
+    }
     public boolean hasStartedExecution() {
         return startedExecution;
     }
